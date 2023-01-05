99f73105628ab66f525e68bb952db208dc2b787f
ShamanShoma <shamanshoma@icloud.com>
2023-01-05 21:07:46 +0500

    problem solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    n = String(n);
+    if (n.slice(0, 1) == '-') {
+        n = n.substr(1).split('').reverse().join('');
+    } else {
+        n = n.split('').reverse().join('');
+    }
+
+    return n;
 }
